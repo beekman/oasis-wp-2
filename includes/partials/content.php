788d4f4deca41f9366efbd601462d96626cbf4b0
Ben Beekman <ben@benbeekman.com>
2016-10-25 17:29:37 -0700

    Changing to supersaas-based scheduling

--- includes/partials/content.php
+++ includes/partials/content.php
@@ -1,16 +1,22 @@
 <?php if (have_rows('tab_panels')): ?>
 	<?php while (have_rows('tab_panels')): ?>
 		<?php
-			the_row();
-			$section_title = get_sub_field('section_title');
-			$section_slug = sanitize_title($section_title);
-		?>
+            the_row();
+            $section_title = get_sub_field('section_title');
+            $section_slug = sanitize_title($section_title);
+        ?>
 
 		<div id="<?=$section_slug?>">
               <h2 class="section-title"><?=the_sub_field('section_title')?></h2>
 			<?=the_sub_field('section_content')?>
 		</div>
 	<?php endwhile; ?>
+<?php endif; ?>
+
+<?php if (is_page('schedule')): ?>
+	<div class="calendar-container">
+		<iframe src="http://www.supersaas.com/schedule/oasis/IndividualSessions" width="600" height="800"> </iframe>
+	</div>
 <?php endif; ?>
 
 <?php echo the_content(); ?>
---
+++ includes/partials/schedule.php
@@ -0,0 +1,3 @@
+<div class="calendar-container">
+	<iframe src="http://www.supersaas.com/schedule/oasis/IndividualSessions" width="600" height="800"> </iframe>
+</div>
